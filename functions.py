d8a8df75b05926de7c1fb24cad943c5eecac6e14
bouddha <ibouddha1.if@gmail.com>
2024-04-19 08:02:01 +0000

    first commit

---
+++ functions.py
@@ -0,0 +1,55 @@
+import json
+import os
+import yaml
+import pandas as pd
+import xml.etree.ElementTree as ET
+
+def jsontocsv(jsonfile):
+    data = pd.read_json(jsonfile)
+    data.to_csv("./outputs/outputfromjson.csv",index=False)
+    
+def csvtojson(csvfile):
+    data = pd.read_csv(csvfile)
+    records = json.loads(data.to_json(orient="records"))
+    with open("./outputs/outputfromcsv.json","w") as f:
+        data = json.dump(records,f)
+        
+def read(filename):
+    ext = getFormat(filename)
+    match(ext):
+        case "json":
+            data = pd.read_json(filename)
+            
+        case "csv":
+            data = pd.read_csv(filename)
+            
+        case "yaml":
+            with open(filename) as stream:
+                try:
+                    data = yaml.safe_load(stream)
+                except yaml.YAMLError as exc:
+                    print(exc)
+                    
+        case "xml":
+            tree = ET.parse(filename)
+            racine = tree.getroot()
+            
+            data = []
+            for child in racine.findall('book'):
+                title = child.find('title').text
+                author = child.find('author').text
+                year = child.find('year').text
+                data.append([title,author,year])
+                
+        case _:
+            print("Invalid file format")
+            
+    return data
+
+def getFormat(filename):
+    """
+    Returns the format of a file.
+    """
+    return os.path.splitext(filename)[1][1:]
+
+print(read("./books/books.yaml"))
